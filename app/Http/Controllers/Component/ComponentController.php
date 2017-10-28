ba3607b86c7a3fc12d51ef5c54cad9bf4a483507
Daniel BC <danibeca@okazo.co>
2017-10-27 23:17:13 -0500

    Fixing component index method

--- app/Http/Controllers/Component/ComponentController.php
+++ app/Http/Controllers/Component/ComponentController.php
@@ -7,6 +7,7 @@ use App\Models\Component\Component;
 use App\Models\Component\ComponentTree;
 use App\Utils\Transformers\SimpleComponentTransformer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 
 class ComponentController extends ApiController
