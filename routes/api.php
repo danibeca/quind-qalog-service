ac7e648a6130e63ce603728c5fb31c46cf4a15e9
Daniel BC <danibeca@okazo.co>
2017-11-05 22:24:33 -0500

    Removing security on docker file.

--- routes/api.php
+++ routes/api.php
@@ -11,67 +11,78 @@
 |
 */
 $router->group([
-    'prefix' => 'api/v1', 'middleware' => ['auth','cors', 'log', 'lang']], function () use ($router) {
-
+    'prefix' => 'api/v1', 'middleware' => ['cors', 'log', 'lang']], function () use ($router) {
 
     $router->group([
         'prefix'    => '/components',
         'namespace' => 'Component'], function () use ($router) {
-
-        $router->get('/', ['uses' => 'ComponentController@index']);
-        $router->post('/', ['uses' => 'ComponentController@store']);
-        $router->put('/{id:[\d]+}', ['uses' => 'ComponentController@update']);
-        $router->delete('/{id:[\d]+}', ['uses' => 'ComponentController@destroy']);
-
-        $router->get('/{id:[\d]+}/leaves', ['uses' => 'ComponentLeafController@index']);
-
-        $router->post('/{id:[\d]+}/metric-values', ['uses' => 'ComponentMetricValueController@create']);
-        $router->post('/{id:[\d]+}/issue-values', ['uses' => 'ComponentIssueValueController@create']);
-
-        $router->get('/{id:[\d]+}/quality-system-instances', ['uses' => 'ComponentQualitySystemController@index']);
 
         $router->get('/{id:[\d]+}/docker', ['uses' => 'DockerComposeController@index']);
 
     });
 
     $router->group([
-        'prefix'    => '/indicators',
-        'namespace' => 'Component'], function () use ($router) {
+        'middleware' => ['auth']], function () use ($router) {
 
-        $router->get('/', ['uses' => 'IndicatorController@index']);
+        $router->group([
+            'prefix'    => '/components',
+            'namespace' => 'Component'], function () use ($router) {
 
-    });
+            $router->get('/', ['uses' => 'ComponentController@index']);
+            $router->post('/', ['uses' => 'ComponentController@store']);
+            $router->put('/{id:[\d]+}', ['uses' => 'ComponentController@update']);
+            $router->delete('/{id:[\d]+}', ['uses' => 'ComponentController@destroy']);
 
-    $router->group([
-        'prefix'    => '/metrics',
-        'namespace' => 'Metric'], function () use ($router) {
+            $router->get('/{id:[\d]+}/leaves', ['uses' => 'ComponentLeafController@index']);
 
-        $router->get('/', ['uses' => 'ExternalMetricController@index']);
-    });
+            $router->post('/{id:[\d]+}/metric-values', ['uses' => 'ComponentMetricValueController@create']);
+            $router->post('/{id:[\d]+}/issue-values', ['uses' => 'ComponentIssueValueController@create']);
 
-    $router->group([
-        'prefix'    => '/quality-systems',
-        'namespace' => 'QualitySystem'], function () use ($router) {
-        $router->get('/', ['uses' => 'QualitySystemController@index']);
-    });
+            $router->get('/{id:[\d]+}/quality-system-instances', ['uses' => 'ComponentQualitySystemController@index']);
 
-    $router->group([
-        'prefix'    => '/quality-system-instances',
-        'namespace' => 'QualitySystem'], function () use ($router) {
-        $router->get('/', ['uses' => 'QualitySystemInstanceController@index']);
-        $router->get('/{id:[\d]+}', ['uses' => 'QualitySystemInstanceController@show']);
-        $router->get('/verify', ['uses' => 'QualitySystemInstanceController@verify']);
-        $router->post('/', ['uses' => 'QualitySystemInstanceController@store']);
-        $router->put('/{id:[\d]+}', ['uses' => 'QualitySystemInstanceController@update']);
-    });
+            $router->get('/{id:[\d]+}/docker', ['uses' => 'DockerComposeController@index']);
 
-    $router->group([
-        'prefix'    => '/api-clients',
-        'namespace' => 'APIClient'], function () use ($router) {
-        $router->get('/{code}/roots', ['uses' => 'APIClientComponentController@index']);
-        $router->post('/{id:[\d]+}/roots', ['uses' => 'APIClientComponentController@update']);
-        $router->get('/{code}', ['uses' => 'APIClientController@show']);
-        $router->post('/{code}/resources', ['uses' => 'APIClientResourceController@store']);
+        });
+
+        $router->group([
+            'prefix'    => '/indicators',
+            'namespace' => 'Component'], function () use ($router) {
+
+            $router->get('/', ['uses' => 'IndicatorController@index']);
+
+        });
+
+        $router->group([
+            'prefix'    => '/metrics',
+            'namespace' => 'Metric'], function () use ($router) {
+
+            $router->get('/', ['uses' => 'ExternalMetricController@index']);
+        });
+
+        $router->group([
+            'prefix'    => '/quality-systems',
+            'namespace' => 'QualitySystem'], function () use ($router) {
+            $router->get('/', ['uses' => 'QualitySystemController@index']);
+        });
+
+        $router->group([
+            'prefix'    => '/quality-system-instances',
+            'namespace' => 'QualitySystem'], function () use ($router) {
+            $router->get('/', ['uses' => 'QualitySystemInstanceController@index']);
+            $router->get('/{id:[\d]+}', ['uses' => 'QualitySystemInstanceController@show']);
+            $router->get('/verify', ['uses' => 'QualitySystemInstanceController@verify']);
+            $router->post('/', ['uses' => 'QualitySystemInstanceController@store']);
+            $router->put('/{id:[\d]+}', ['uses' => 'QualitySystemInstanceController@update']);
+        });
+
+        $router->group([
+            'prefix'    => '/api-clients',
+            'namespace' => 'APIClient'], function () use ($router) {
+            $router->get('/{code}/roots', ['uses' => 'APIClientComponentController@index']);
+            $router->post('/{id:[\d]+}/roots', ['uses' => 'APIClientComponentController@update']);
+            $router->get('/{code}', ['uses' => 'APIClientController@show']);
+            $router->post('/{code}/resources', ['uses' => 'APIClientResourceController@store']);
+        });
     });
 });
 
